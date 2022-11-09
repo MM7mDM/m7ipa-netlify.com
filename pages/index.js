fcf733a6de9d1f235b2d10c189b7b4274ad18ad6
MM7mDM <111407171+MM7mDM@users.noreply.github.com>
2022-11-10 00:52:49 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="اهلا بك في" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          موقع بلس لتحميل اشياء مهكره ومجانية ومحذوفة</code>
         </p>
       </main>
 
